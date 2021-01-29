b13f2971d28a8cb3ec8afe1c4dc4f6b74986cea6
jtreutel <jennings@circleci.com>
2021-01-29 17:44:58 +0900

    Add Terraform plans

---
+++ terraform/ecr/main.tf
@@ -0,0 +1,3 @@
+resource "aws_ecr_repository" "nodedemo" {
+  name = "jennings-node-demo"
+}
---
+++ terraform/route53/main.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "nodedemo" {
+  name         = "jennings-circleci20.com"
+  private_zone = true
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.nodedemo.zone_id
+  name    = "nodedemo.${data.aws_route53_zone.nodedemo.name}"
+  type    = "A"
+  ttl     = "30"
+  records = ["10.0.0.1"]
+}
