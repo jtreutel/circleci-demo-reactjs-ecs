4c8ff148f5f93dd0b6317646f17191f974470e0b
jtreutel <jennings@circleci.com>
2021-01-29 21:40:13 +0900

    Fix SG rule.

--- tf/infra/main.tf
+++ tf/infra/main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "nodedemo_alb" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
