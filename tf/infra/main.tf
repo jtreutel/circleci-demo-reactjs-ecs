f1ffd2613d4ab648a4db9ef3f24f5926cf6c0019
jtreutel <jennings@circleci.com>
2021-01-29 21:30:13 +0900

    Add additional ALB and ECS resources.

--- tf/infra/main.tf
+++ tf/infra/main.tf
@@ -27,3 +27,72 @@ resource "aws_ecs_task_definition" "nodedemo" {
 resource "aws_ecs_cluster" "nodedemo" {
   name = "${var.aws_resource_name_prefix}-cluster"
 }
+
+
+
+
+
+resource "aws_ecs_service" "nodedemo" {
+  name            = "${var.aws_resource_name_prefix}-service"
+  cluster         = aws_ecs_cluster.nodedemo.id
+  task_definition = aws_ecs_task_definition.nodedemo.arn
+  desired_count   = 2
+
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.nodedemo.arn
+    container_name   = "${var.aws_resource_name_prefix}-service"
+    container_port   = 3000
+  }
+
+}
+
+
+resource "aws_security_group" "nodedemo_alb" {
+  name        = "${var.aws_resource_name_prefix}-lb-sg"
+  description = "Node app on ECS demo"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_lb" "nodedemo" {
+  name               = "${var.aws_resource_name_prefix}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.nodedemo_alb.id]
+  subnets            = var.public_subnet_id
+}
+
+resource "aws_lb_listener" "nodedemo" {
+  load_balancer_arn = aws_lb.nodedemo.arn
+  port              = "80"
+  protocol          = "HTTP"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nodedemo.arn
+  }
+}
+
+resource "aws_lb_target_group" "nodedemo" {
+  name     = "${var.aws_resource_name_prefix}-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
