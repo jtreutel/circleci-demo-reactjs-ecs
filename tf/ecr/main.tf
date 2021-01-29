a23553262e8d4a3ca524e1f29c24592524e8eebf
jtreutel <jennings@circleci.com>
2021-01-29 18:08:24 +0900

    Add missing var

--- tf/ecr/main.tf
+++ tf/ecr/main.tf
@@ -1,3 +1,8 @@
+data "aws_route53_zone" "nodedemo" {
+  name         = "jennings-circleci20.com"
+  private_zone = true
+}
+
 resource "aws_ecr_repository" "nodedemo" {
-  name = "jennings-node-demo"
+  name = "${var.aws_resource_name_prefix}-ecr"
 }
---
+++ tf/ecr/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_resource_name_prefix" {
+    type = string
+    default = "defaultprefix"
+}
