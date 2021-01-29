d8aac5649f980d37fb0cdafbb77296b478dea1ad
jtreutel <jennings@circleci.com>
2021-01-29 19:16:26 +0900

    Rename folder; add dummy task def.

--- tf/ecr/main.tf
+++
@@ -1,7 +0,0 @@
-data "aws_route53_zone" "nodedemo" {
-  name = "jennings-circleci20.com"
-}
-
-resource "aws_ecr_repository" "nodedemo" {
-  name = "${var.aws_resource_name_prefix}-ecr"
-}
---
+++ tf/infra/main.tf
@@ -0,0 +1,16 @@
+data "aws_route53_zone" "nodedemo" {
+  name = "jennings-circleci20.com"
+}
+
+resource "aws_ecr_repository" "nodedemo" {
+  name = "${var.aws_resource_name_prefix}-ecr"
+}
+
+resource "aws_ecs_task_definition" "nodedemo" {
+  family                = "${var.aws_resource_name_prefix}-service"
+  container_definitions = file("task-definitions/service.json")
+}
+
+resource "aws_ecs_cluster" "nodedemo" {
+  name = "${var.aws_resource_name_prefix}-cluster"
+}
