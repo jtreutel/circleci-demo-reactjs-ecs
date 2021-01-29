3e7059927ecca4fcdb4bbf6ccc78f97011c52be3
jtreutel <jennings@circleci.com>
2021-01-29 20:49:10 +0900

    Update task def.

--- tf/infra/main.tf
+++ tf/infra/main.tf
@@ -1,6 +1,11 @@
+#Check if hosted zone for target domain exists
 data "aws_route53_zone" "nodedemo" {
   name = "jennings-circleci20.com"
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
 
 resource "aws_ecr_repository" "nodedemo" {
   name = "${var.aws_resource_name_prefix}-ecr"
@@ -11,8 +16,10 @@ resource "aws_ecs_task_definition" "nodedemo" {
   container_definitions = templatefile(
     "${path.module}/task-definitions/service.json.tpl",
     {
-      image_name = "${var.aws_resource_name_prefix}",
-      image_tag  = "${var.commit_hash}"
+      image_name  = "${var.aws_resource_name_prefix}",
+      image_tag   = "${var.commit_hash}",
+      aws_acct_no = "${data.aws_caller_identity.current.id}",
+      aws_region  = "${data.aws_region.current.name}"
     }
   )
 }
--- tf/infra/task-definitions/service.json.tpl
+++ tf/infra/task-definitions/service.json.tpl
@@ -1,9 +1,16 @@
 [
   {
-    "name": "nodedemo",
-    "image": "123456789012.dkr.ecr.us-west-2.amazonaws.com/${image_name}:${image_tag}",
+    "name": "${image_name}-service",
+    "image": "${aws_acct_no}.dkr.ecr.${aws_region}.amazonaws.com/${image_name}:${image_tag}",
     "memory": 200,
     "cpu": 10,
-    "essential": true
+    "essential": true,
+    "portMappings": [
+      {
+        "hostPort": 3000,
+        "protocol": "tcp",
+        "containerPort": 3000
+      }
+    ],
   }
 ]
